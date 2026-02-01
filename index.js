d901895440af0d55d570e2c79534cca0b6bff3d9
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-01 17:02:30 +0600

    added practice 2

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-function calculateMoney(ticketSale) {
-  if (ticketSale <= 0) return "Ticket sale must be greater than 0";
-  const totalMoney = ticketSale * 120;
-  const payGuard = 500;
-  const lunch = 8 * 50;
-  const deduct = payGuard + lunch;
-  const output = totalMoney - deduct;
-  return output;
+// Practice 1
+
+// function calculateMoney(ticketSale) {
+//   if (ticketSale <= 0) return "Ticket sale must be greater than 0";
+//   const totalMoney = ticketSale * 120;
+//   const payGuard = 500;
+//   const lunch = 8 * 50;
+//   const deduct = payGuard + lunch;
+//   const output = totalMoney - deduct;
+//   return output;
+// }
+
+// const ticketSale = 1055;
+// const restMoney = calculateMoney(ticketSale);
+// console.log(restMoney);
+
+// Practice 2
+
+function checkName(name) {
+  if (typeof name !== "string" || name.length === 0) return "Invalid";
+  const lastChar = name[name.length - 1].toLowerCase();
+  if (
+    lastChar === "a" ||
+    lastChar === "y" ||
+    lastChar === "i" ||
+    lastChar === "e" ||
+    lastChar === "o" ||
+    lastChar === "u" ||
+    lastChar === "w"
+  ) {
+    return "Good Name";
+  }
+  return "Bad Name";
 }
 
-const ticketSale = 1055;
-const restMoney = calculateMoney(ticketSale);
-console.log(restMoney);
+const name = "Fahad";
+const result = checkName(name);
+console.log(result);
